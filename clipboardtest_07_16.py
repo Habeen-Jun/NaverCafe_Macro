67af3e39ada00eac6324b18be699f235d13f45ca
HabeenJun <junhabin@gmail.com>
2020-07-21 10:25:01 +0900

    ,

--- clipboardtest_07_16.py
+++
@@ -1,8 +0,0 @@
-## 기존에 쓰던 pyperclip 모듈은 image 호환 안됨..
-## PIL (Python Image Library)
-from PIL import ImageGrab
-
-img = ImageGrab.grabclipboard()
-# or ImageGrab.grab() to grab the whole screen!
-
-print(img.show())
