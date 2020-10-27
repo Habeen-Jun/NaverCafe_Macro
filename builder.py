20b3b4a841b5e3cdd19f39138832b17e57e3545a
Habeen-Jun <61823940+Habeen-Jun@users.noreply.github.com>
2020-10-27 11:22:25 +0900

    create builder.py

--- builder.py
+++ builder.py
@@ -2,10 +2,10 @@ import os
 
 
 def build_exe(debug_mode=False):
-    if debug_mode == False:
+    if debug_mode == True:
         os.system('pyinstaller main.py --onefile --add-binary "./driver/chromedriver.exe;./driver" --add-data "./ui_files/login.ui;./ui_files" --add-data "./ui_files/editwindow.ui;./ui_files" --add-data "./ui_files/mainwindow_ex.ui;./ui_files" --add-data "./ui_files/naverlogin.ui;./ui_files" --icon="icon.ico"')
     else:
-        os.system('pyinstaller main.py --onefile --add-binary --noconsole\
+        os.system('pyinstaller main.py --onefile --noconsole --add-binary \
             "./driver/chromedriver.exe;./driver" --add-data \
                 "./ui_files/login.ui;./ui_files" --add-data \
                     "./ui_files/editwindow.ui;./ui_files" \
